28d328f1ed068217c07732f4d57484d28220ff56
JJiWoNing <jjiwoning12@gmail.com>
2023-07-19 23:46:08 +0900

    3장. 템플릿

---
+++ src/main/java/com/example/tobyspring/user/dao/template/class_seperate/JdbcContext.java
@@ -0,0 +1,46 @@
+package com.example.tobyspring.user.dao.template.class_seperate;
+
+import com.example.tobyspring.user.dao.template.strategy.StatementStrategy;
+
+import javax.sql.DataSource;
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.SQLException;
+
+public class JdbcContext {
+
+    private final DataSource dataSource;
+
+    public JdbcContext(DataSource dataSource) {
+        this.dataSource = dataSource;
+    }
+
+    public void workWithStatementStrategy(StatementStrategy stmt) throws SQLException {
+        Connection c = null;
+        PreparedStatement ps = null;
+
+        try {
+            c = this.dataSource.getConnection();
+
+            ps = stmt.makePrepareStatement(c);
+
+            ps.executeUpdate();
+        } finally {
+            if (ps != null) {
+                try {
+                    ps.close();
+                } catch (SQLException e) {
+                    e.printStackTrace();
+                }
+            }
+            if (c != null) {
+                try {
+                    c.close();
+                } catch (SQLException e) {
+                    e.printStackTrace();
+                }
+            }
+        }
+
+    }
+}
