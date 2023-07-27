a53f9de327361b5001eea7d3b66074d04dbf1262
JJiWoNing <jjiwoning12@gmail.com>
2023-07-27 23:10:45 +0900

    3장. 템플릿

---
+++ src/main/java/com/example/tobyspring/user/dao/template/jdbctemplate/UserDaoJdbcTemplate.java
@@ -0,0 +1,28 @@
+package com.example.tobyspring.user.dao.template.jdbctemplate;
+
+import com.example.tobyspring.user.domain.User;
+import org.springframework.jdbc.core.JdbcTemplate;
+import org.springframework.jdbc.core.PreparedStatementCreator;
+
+import javax.sql.DataSource;
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.SQLException;
+
+public class UserDaoJdbcTemplate {
+
+    private final JdbcTemplate jdbcTemplate;
+
+    public UserDaoJdbcTemplate(DataSource dataSource) {
+        this.jdbcTemplate = new JdbcTemplate(dataSource);
+    }
+
+    public void deleteAll() {
+        jdbcTemplate.update("delete from users");
+    }
+
+    public void add(User user) {
+        jdbcTemplate.update("insert into users(id, name, password) values (?, ?, ?)",
+                user.getId(), user.getName(), user.getPassword());
+    }
+}
